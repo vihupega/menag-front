7bf589f4bfc8edae9f6c0fc08c9c6dc814e40895
unknown <vihupega@gmail.com>
2022-02-07 17:41:11 -0600

    Plantilla base del front publico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ const routes: Routes = [
   
   {
     path:'',
-    redirectTo:'home',
+    redirectTo:'',
     pathMatch:'full'
   },
   {
     path:'**',
-    redirectTo:'home',
+    redirectTo:'',
     pathMatch:'full'
   }
 ];
